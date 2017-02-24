39a1bbba1c4a2c9cde448043a72b608158aaca42
tyunkinilya <tyunkinilya@gmail.com>
2017-02-24 17:09:54 +0300

    Main

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # -*- coding: UTF-8 -*-
 import itevents
-# import search
 import stemmer
 
 basic_url = 'http://it-events.com/events/'
 
-event_num = 999
+event_num = 8522
 event = itevents.process_url(basic_url + str(event_num))
-# itevents.write_event_txt(event, 'output.txt', 'w')
+itevents.write_event_txt(event, 'output.txt', 'w')
 # itevents.write_event_json(event, 'output.json', 'w')
-# print(str(itevents.event_to_json(event)))
 
 def find_phrase(phrase, event):
 	ph = stemmer.stem_phrase(phrase)
