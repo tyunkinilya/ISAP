41f7b01f9b40f29631401d56a8f56d009ecc1e9c
tyunkinilya <tyunkinilya@gmail.com>
2017-02-24 14:02:48 +0300

    Main
    Added itevents

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
- 
-# Some code
- 
+# -*- coding: UTF-8 -*-
+import itevents
+
+basic_url = 'http://it-events.com/events/'
+
+event_num = 8750
+event = itevents.process_url(basic_url + str(event_num))
+itevents.write_event_txt(event, 'output.txt', 'w')
+itevents.write_event_json(event, 'output.json', 'w')
+print(str(itevents.event_to_json(event)))
